23df7d8a19d29a4d3d974d11a80743c6111f3f42
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-26 18:20:36 +0330

    test TA_Handler

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from tradingview_ta import TA_Handler, Interval, Exchange
+tesla = TA_Handler(
+    symbol="TSLA",
+    screener="america",
+    exchange="NASDAQ",
+    interval=Interval.INTERVAL_1_DAY,
+    # proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
+)
+print(tesla.get_analysis().summary)
