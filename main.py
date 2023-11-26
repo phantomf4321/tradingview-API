a4606dd824b460237d0b60fdef89ba12c93a2bc5
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-26 18:26:57 +0330

    test WebSocket

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
-tesla = TA_Handler(
-    symbol="TSLA",
-    screener="america",
-    exchange="NASDAQ",
-    interval=Interval.INTERVAL_1_DAY,
-    # proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
-)
-print(tesla.get_analysis().summary)
+import json
+import random
+import re
+import string
+
+import requests
+from websocket import *
+
+print(WebSocket)
